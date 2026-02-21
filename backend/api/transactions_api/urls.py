04a05315f8306901ff33cc6f9dbb0f0ed12b5f76
4grotten <barsbekclash@gmail.com>
2026-02-22 00:02:26 +0600

    register urls inside transactions

--- backend/api/transactions_api/urls.py
+++ backend/api/transactions_api/urls.py
@@ -1,22 +1,21 @@
 from django.urls import path
-from .views import (
-    BankTopupView, CryptoTopupView, 
-    CardTransferView, CryptoWithdrawalView, 
-    BankWithdrawalView, TransactionReceiptView
-)
-
-
-app_name = 'api.transactions_api'
-
+from . import views
 
 urlpatterns = [
-    path('topup/bank/', BankTopupView.as_view(), name='bank_topup'),
-    path('topup/crypto/', CryptoTopupView.as_view(), name='crypto_topup'),
-    
-    path('transfer/card/', CardTransferView.as_view(), name='card_transfer'),
-    
-    path('withdrawal/crypto/', CryptoWithdrawalView.as_view(), name='crypto_withdrawal'),
-    path('withdrawal/bank/', BankWithdrawalView.as_view(), name='bank_withdrawal'),
+    path('topup/bank/', views.BankTopupView.as_view(), name='bank_topup'),
+    path('topup/crypto/', views.CryptoTopupView.as_view(), name='crypto_topup'),
+    path('transfer/card/', views.CardTransferView.as_view(), name='card_transfer'),
+    path('withdrawal/crypto/', views.CryptoWithdrawalView.as_view(), name='crypto_withdrawal'),
+    path('withdrawal/bank/', views.BankWithdrawalView.as_view(), name='bank_withdrawal'),
+    path('<uuid:transaction_id>/receipt/', views.TransactionReceiptView.as_view(), name='transaction_receipt'),
 
-    path('<uuid:transaction_id>/receipt/', TransactionReceiptView.as_view(), name='transaction_receipt'),
+
+    path('all/', views.AllTransactionsListView.as_view(), name='all_transactions'),
+    path('iban/', views.IBANTransactionsListView.as_view(), name='iban_transactions'),
+    path('card-transactions/', views.CardTransactionsListView.as_view(), name='card_transactions'),
+
+
+    path('recipient-info/', views.RecipientInfoView.as_view(), name='recipient_info'),
+    path('bank-accounts/', views.UserBankAccountsView.as_view(), name='user_bank_accounts'),
+    path('crypto-wallets/', views.UserCryptoWalletsView.as_view(), name='user_crypto_wallets'),
 ]
